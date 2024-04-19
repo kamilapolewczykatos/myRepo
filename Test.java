d948371f2dab916bcba9c9d3b651376388863c57
Kamila Polewczyk-Sońta <104437521+kamilapolewczykatos@users.noreply.github.com>
2024-04-19 12:43:33 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test {
   public static void main(String arg[]) {
     System.out.println("Hello Java ");
-System.out.println("Hello Java 231");
+System.out.println("Hello Java 2341");
   }
 }
